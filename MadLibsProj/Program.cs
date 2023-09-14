d34ad477626c62f8d9310172f46f259ac1a7e23e
Yvette Lerma <lerma.yvette@hotmail.com>
2023-09-14 14:19:21 -0600

    built without tutorial

--- MadLibsProj/Program.cs
+++ MadLibsProj/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Provide a verb");
+        var verb = Console.ReadLine();
+
+        Console.WriteLine("Provide a noun");
+        var noun = Console.ReadLine();
+
+        Console.WriteLine("Pick a number between 1-10.");
+        var number = Console.ReadLine();
+
+        Console.WriteLine($"I went for a {verb}. When I saw a {noun}, " +
+            $"and I couldn't believe my eyes. They had {number} chickens in their pocket.");
+    }
+    
+}
 
